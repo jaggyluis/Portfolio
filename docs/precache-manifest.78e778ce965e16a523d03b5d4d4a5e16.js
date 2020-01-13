3145138c20cd0b3d71199a1e8fc77b44e8618181
jaggyluis <luis.jaggy@gmail.com>
2020-01-12 20:55:31 -0800

    added docs and build

---
+++ docs/precache-manifest.78e778ce965e16a523d03b5d4d4a5e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ff0be0c8891c2bf33fc72a74e4c6ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa6dd3a73cea4c525c0",
+    "url": "/static/css/main.eaa3a720.chunk.css"
+  },
+  {
+    "revision": "56e06f394ad367b5bf85",
+    "url": "/static/js/2.c0d86c32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c0d86c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afa6dd3a73cea4c525c0",
+    "url": "/static/js/main.e7d869f9.chunk.js"
+  },
+  {
+    "revision": "bc0a437ac56b6d303c33",
+    "url": "/static/js/runtime-main.ca2d27e8.js"
+  }
+]);
