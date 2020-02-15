16a35df93b500952be8143dc6fdfe830cc91005c
jaggyluis <luis.jaggy@gmail.com>
2020-02-14 18:29:58 -0800

    re-adding imgs

---
+++ docs/precache-manifest.d0f4bd841cd90c5a75e3bbc70a74e687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb2a54858980e9ceb9e702be667b34e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5cabf0126080e694cca",
+    "url": "./static/css/main.99bbea07.chunk.css"
+  },
+  {
+    "revision": "56e06f394ad367b5bf85",
+    "url": "./static/js/2.c0d86c32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c0d86c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5cabf0126080e694cca",
+    "url": "./static/js/main.91c57c7c.chunk.js"
+  },
+  {
+    "revision": "f786f926713acc0226d2",
+    "url": "./static/js/runtime-main.8d70acf7.js"
+  }
+]);
